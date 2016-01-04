1b3802f7e60e2b3f20614c2fbdaf3a8d7b691554
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2016-01-04 20:47:05 +0100

    Exit & print error if current dir is not a git dir

--- src/main/java/de/xsrc/scm/App.java
+++ src/main/java/de/xsrc/scm/App.java
@@ -2,6 +2,9 @@ package de.xsrc.scm;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.googlecode.lanterna.TerminalFacade;
 import com.googlecode.lanterna.gui.Border;
 import com.googlecode.lanterna.gui.GUIScreen;
@@ -19,51 +22,59 @@ import de.xsrc.scm.tui.HistoryWindow;
  *
  */
 public class App {
+
+  private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
   public static void main(final String[] args) throws IOException {
     final GUIScreen textGUI = TerminalFacade.createGUIScreen();
 
-    final History history = new History(System.getProperty("user.dir"));
-    final Window window = new HistoryWindow(history);
-    window.setSoloWindow(true);
-    window.setBorder(new Border.Invisible());
-    window.addWindowListener(new WindowListener() {
+    try {
+      final History history = new History(System.getProperty("user.dir"));
+      final Window window = new HistoryWindow(history);
+      window.setSoloWindow(true);
+      window.setBorder(new Border.Invisible());
+      window.addWindowListener(new WindowListener() {
 
-      @Override
-      public void onFocusChanged(final Window window, final Interactable fromComponent,
-          final Interactable toComponent) {
-        // TODO Auto-generated method stub
+        @Override
+        public void onFocusChanged(final Window window, final Interactable fromComponent,
+            final Interactable toComponent) {
+          // TODO Auto-generated method stub
 
-      }
-
-      @Override
-      public void onUnhandledKeyboardInteraction(final Window window, final Key key) {
-        if (key != null) {
-          System.out.println(key.toString());
-          textGUI.getScreen().stopScreen();
-          System.exit(0);
         }
-      }
 
-      @Override
-      public void onWindowClosed(final Window window) {
-        // TODO Auto-generated method stub
+        @Override
+        public void onUnhandledKeyboardInteraction(final Window window, final Key key) {
+          if (key != null) {
+            System.out.println(key.toString());
+            textGUI.getScreen().stopScreen();
+            System.exit(0);
+          }
+        }
 
-      }
+        @Override
+        public void onWindowClosed(final Window window) {
+          // TODO Auto-generated method stub
 
-      @Override
-      public void onWindowInvalidated(final Window window) {
-        // TODO Auto-generated method stub
+        }
 
-      }
+        @Override
+        public void onWindowInvalidated(final Window window) {
+          // TODO Auto-generated method stub
 
-      @Override
-      public void onWindowShown(final Window window) {
-        // TODO Auto-generated method stub
+        }
 
-      }
-    });
-    textGUI.getScreen().startScreen();
+        @Override
+        public void onWindowShown(final Window window) {
+          // TODO Auto-generated method stub
 
-    textGUI.showWindow(window, GUIScreen.Position.FULL_SCREEN);
+        }
+      });
+      textGUI.getScreen().startScreen();
+
+      textGUI.showWindow(window, GUIScreen.Position.FULL_SCREEN);
+    } catch (final IOException e) {
+      LOG.error("Current dir is not git dir");
+      System.exit(-1);
+    }
   }
 }
